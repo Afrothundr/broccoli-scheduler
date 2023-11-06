19446c61d32aeb1c9763e26c7259d51d07af230e
Branford Harris <branford.harris@bybeam.co>
2023-11-06 09:40:16 -0600

    testing redis connection

--- src/app.ts
+++ src/app.ts
@@ -16,18 +16,22 @@ const client = createClient({
   url: process.env.REDIS_PRIVATE_URL,
 });
 
-const queues = {
-  itemUpdater: new Queue(QUEUE_TYPES.ITEM_UPDATER, {
-    connection: redisOptions,
-  }),
-};
+client.on("error", (err) => {
+  console.error("Redis Error: " + err);
+});
+
+// const queues = {
+//   itemUpdater: new Queue(QUEUE_TYPES.ITEM_UPDATER, {
+//     connection: redisOptions,
+//   }),
+// };
 
 const app = express();
 
 // Utilities
 
-const addJobToItemUpdaterQueue = async (job: WorkerJob, delay: number) =>
-  await queues.itemUpdater.add(job.type, job, { delay });
+// const addJobToItemUpdaterQueue = async (job: WorkerJob, delay: number) =>
+//   await queues.itemUpdater.add(job.type, job, { delay });
 
 (async () => {
   try {
@@ -39,16 +43,16 @@ const addJobToItemUpdaterQueue = async (job: WorkerJob, delay: number) =>
         res
       ) => {
         const { ids, status, delay } = req.body;
-        await addJobToItemUpdaterQueue(
-          {
-            type: jobTypes.ITEM_UPDATER,
-            data: { ids, status },
-          },
-          delay
-        );
-        res.status(200).json({
-          queued: true,
-        });
+        // await addJobToItemUpdaterQueue(
+        //   {
+        //     type: jobTypes.ITEM_UPDATER,
+        //     data: { ids, status },
+        //   },
+        //   delay
+        // );
+        // res.status(200).json({
+        //   queued: true,
+        // });
       }
     );
     app.listen(process.env.PORT, async () => {
@@ -61,8 +65,3 @@ const addJobToItemUpdaterQueue = async (job: WorkerJob, delay: number) =>
     console.error("error on startup:", e);
   }
 })();
-
-client.on("error", (err) => {
-  console.log("Error occurred while connecting or accessing redis server", err);
-  throw err;
-});
